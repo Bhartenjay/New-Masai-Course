44bbbcb95fdf07863bba4ccf24779d5174b05efd
Bhartenjay Giri <bhartenjaygiri@gmail.com>
2024-12-18 16:14:15 +0530

    update object

---
+++ Day -1/FEDC-24012_S01  Revisiting JS  Arrays , Strings & Objects/Q.13/index.js
@@ -0,0 +1,14 @@
+let budget = 
+{
+    income: 1000,
+   expenses: 700
+}
+
+budget["saving"] = 300;
+
+console.log(budget);
+
+budget["income"] = 1200;
+budget["saving"] = 500;
+
+console.log(budget);
